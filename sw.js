2e8184cb3555284e20c178052fe9431d2bdd7c55
nakpongbantita-sketch <nakpongbantita@gmail.com>
2026-03-19 13:35:39 +0700

    Update cache name and optimize event listeners

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-const cacheName = 'lotto-app-v1';
+const cacheName = 'lotto-v2026';
 const assets = [
   './',
   './index.html',
-  'https://cdn-icons-png.flaticon.com/512/2953/2953361.png'
+  './manifest.json',
+  './icon.png'
 ];
 
-// ติดตั้งและเก็บไฟล์ลง Cache
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// ดึงข้อมูลจาก Cache เมื่อ Offline
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
